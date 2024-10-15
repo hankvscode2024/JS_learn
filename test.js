97806fad68ab74b7142094c5dbdc799f29a980f9
hankvscode2024 <harshhappy2812@gmail.com>
2024-10-15 12:22:33 +0530

    adding 1st hour

---
+++ test.js
@@ -0,0 +1,24 @@
+console.log("hi harsh")
+
+let iceCream = "chocolate";
+if (iceCream === "chocolate") {
+  alert("Yay, I love chocolate ice cream!");
+} else {
+  alert("Awwww, but chocolate is my favorite…");
+}
+const myHeading = document.querySelector("h1");
+myHeading.textContent = "Hello world!";
+
+let myVariable = "Bob";
+myVariable=10;
+console.log(myVariable);
+
+function multiply(num1, num2) {
+    let result = num1 * num2;
+    return result;
+  }
+
+let mux=multiply(10,8);
+console.log(mux+2);
+
+document.querySelector("h1").addEventListener("click",()=> {alert("don't  click me")});
